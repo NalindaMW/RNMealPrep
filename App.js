de522d5f487cb997c8e71011cad1c4322b0270ad
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-14 13:05:26 +0530

    + Adding Navigation header styles

--- App.js
+++ App.js
@@ -13,9 +13,23 @@ export default function App() {
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#24180f" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{ title: "All Categories" }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{ title: "Meals Overview", headerBackTitle: " " }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
